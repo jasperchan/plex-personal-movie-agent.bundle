3ea7b57442810f39d5d5c0e467aed88a44c5c5dd
Jasper Chan <jasperchan@gmail.com>
2025-04-11 17:46:33 -0700

    Add contributes_to attribute for PlexPersonalMovieAgent

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,7 @@
 class PlexPersonalMovieAgent(Agent.Movies):
     name = 'Personal Movies'
     languages = [Locale.Language.NoLanguage]
+    contributes_to = ['com.plexapp.agents.localmedia', 'com.plexapp.agents.none']
 
     def search(self, results, media, lang):
         results.Append(MetadataSearchResult(id=media.id, name=media.name, year=media.year, lang=lang, score=100))
